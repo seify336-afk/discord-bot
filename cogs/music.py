83a946215406b5bc2ce0c327c6a037fd04e8a267
seify336-afk <seify336@email.com>
2026-06-28 11:49:04 +0300

    fix ffmpeg path

--- cogs/music.py
+++ cogs/music.py
@@ -21,6 +21,7 @@ YTDL_OPTIONS = {
 }
 
 FFMPEG_OPTIONS = {
+    "executable": "ffmpeg",
     "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
     "options": "-vn -ar 48000",
 }
